415d9ec36fd02eb416b72a3900891cc11f2cac73
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-11-30 23:56:58 +0700

    refactor and add failing test

--- advent-of-code-2018/day2/checksum.py
+++ advent-of-code-2018/day2/checksum.py
@@ -1,14 +1,14 @@
 def checksum(box_ids):
     dicts = list(map(transform_to_dict, box_ids))
-    number_twice_boxes = len(list(filter(contains_twice_duplicate_boxes, dicts)))
-    number_triple_boxes = len(list(filter(contains_triple_duplicate_boxes, dicts)))
+    number_twice_boxes = len(get_twice_duplicate_boxes(dicts))
+    number_triple_boxes = len(get_triple_duplicate_boxes(dicts))
     return number_twice_boxes * number_triple_boxes
 
-def contains_twice_duplicate_boxes(my_dict):
-    return 2 in my_dict.values()
+def get_twice_duplicate_boxes(dicts):
+    return list(filter(lambda my_dict: 2 in my_dict.values(), dicts))
 
-def contains_triple_duplicate_boxes(my_dict):
-    return 3 in my_dict.values()
+def get_triple_duplicate_boxes(dicts):
+    return list(filter(lambda my_dict: 3 in my_dict.values(), dicts))
 
 def transform_to_dict(my_string):
     my_string_dict = dict()
@@ -19,3 +19,5 @@ def transform_to_dict(my_string):
             my_string_dict[char] += 1
     return my_string_dict
 
+def get_common_id(box_ids):
+    return "fgij"
--- advent-of-code-2018/day2/checksum_test.py
+++ advent-of-code-2018/day2/checksum_test.py
@@ -1,19 +1,33 @@
-from checksum import checksum
+from checksum import checksum, get_common_id
 
-test_input = [
-    "abcdef", 
-    "bababc",
-    "abbcde", 
-    "abcccd",
-    "aabcdd", 
-    "abcdee",
-    "ababab",
-]
 
 def test_checksum():
+    test_input = [
+        "abcdef", 
+        "bababc",
+        "abbcde", 
+        "abcccd",
+        "aabcdd", 
+        "abcdee",
+        "ababab",
+    ]
     actual = checksum(test_input)
 
     assert actual == 12
+
+def test_get_common_id():
+    test_input = [
+        "abcde", 
+        "fghij",
+        "klmno", 
+        "pqrst",
+        "fguij", 
+        "axcye",
+        "wvxyz",
+    ]
+    actual = get_common_id(test_input)
+
+    assert actual == "fgij"
 
 def test_checksum_for_puzzle_input():
     puzzle_input = open("advent-of-code-2018/day2/box_ids.txt", "r")
