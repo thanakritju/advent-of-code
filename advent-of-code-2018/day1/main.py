ab29ccc47c7f9bc9e9674ee8b380f9e9f520d8de
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-11-30 23:18:46 +0700

    complete day 2 omg im so dumb

--- advent-of-code-2018/day1/main.py
+++ advent-of-code-2018/day1/main.py
@@ -1,2 +1,22 @@
 def calculate_frequency(frequency_list):
-	return sum(map(int, frequency_list))
+	return sum(cast_int(frequency_list))
+
+def cast_int(frequency_list):
+	return map(int, frequency_list)
+
+def calculate_first_reaches_twice(frequency_list):
+	sums = set([0])
+	length = len(frequency_list)
+	last_sum = 0
+	i = 0
+	while True:
+		current_value = last_sum + int(frequency_list[i % length])
+		if current_value in sums:
+			return current_value
+		else:
+			sums.add(current_value)
+			last_sum = current_value
+			i += 1
+
+def is_duplicate(my_list):
+	return len(my_list) != len(set(my_list))
--- advent-of-code-2018/day1/main_test.py
+++ advent-of-code-2018/day1/main_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from main import calculate_frequency
+from main import calculate_frequency, calculate_first_reaches_twice
 
 
 @pytest.mark.parametrize("test_input,expected", 
@@ -15,12 +15,32 @@ def test_calculate_frequency(test_input, expected):
 
     assert actual == expected
 
+@pytest.mark.parametrize("test_input,expected", 
+    [
+        (["+1", "-2", "+3", "+1"], 2),
+        (["+1", "-1"], 0), 
+        (["+3", "+3", "+4", "-2", "-4"], 10), 
+        (["-6", "+3", "+8", "+5", "-6"], 5),
+        (["+7", "+7", "-2", "-7", "-4"], 14),
+    ]
+)
+def test_calculate_first_reaches_twice(test_input, expected):
+    actual = calculate_first_reaches_twice(test_input)
 
+    assert actual == expected
 
-def test_calculate_frequency_for_long_input():
+def test_calculate_frequency_for_puzzle_input():
     puzzle_input = open("advent-of-code-2018/day1/frequency.txt", "r")
     content = puzzle_input.read().split()
 
     actual = calculate_frequency(content)
 
     assert actual == 538
+
+def test_calculate_frequency_for_puzzle_input():
+    puzzle_input = open("advent-of-code-2018/day1/frequency.txt", "r")
+    content = puzzle_input.read().split()
+
+    actual = calculate_first_reaches_twice(content)
+
+    assert actual == 77271
