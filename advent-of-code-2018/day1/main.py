90ef571ff78729d7e8e3eb8fc1638c4296ef26d4
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-11-30 21:54:40 +0700

    complete 2018 day 1 part 1

---
+++ advent-of-code-2018/day1/main.py
@@ -0,0 +1,2 @@
+def calculate_frequency(frequency_list):
+	return sum(map(int, frequency_list))
--- advent-of-code-2018/day1/main_test.py
+++ advent-of-code-2018/day1/main_test.py
@@ -1,2 +1,26 @@
-def test_example():
-    assert True
+import pytest
+
+from main import calculate_frequency
+
+
+@pytest.mark.parametrize("test_input,expected", 
+    [
+        (["+1", "+1", "+1"], 3), 
+        (["+1", "+1", "-2"], 0), 
+        (["-1", "-2", "-3"], -6),
+    ]
+)
+def test_calculate_frequency(test_input, expected):
+    actual = calculate_frequency(test_input)
+
+    assert actual == expected
+
+
+
+def test_calculate_frequency_for_long_input():
+    puzzle_input = open("advent-of-code-2018/day1/frequency.txt", "r")
+    content = puzzle_input.read().split()
+
+    actual = calculate_frequency(content)
+
+    assert actual == 538
