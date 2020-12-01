af0691f52f9eb180199a17b13520da7aa5437ce6
thanakritju <thanakrit.ju.work@gmail.com>
2020-12-01 15:53:55 +0700

    Day4 still on progress

--- advent-of-code-2018/day4/guard.py
+++ advent-of-code-2018/day4/guard.py
@@ -1,2 +1,40 @@
-def guard(logs):
+import re
+from datetime import datetime
+
+
+class Guard:
+    def __init__(self, id):
+        self.id = id
+        self.is_active = False
+        self.work_time = 0
+
+
+def guard(actions):
+    state = None
+    for action in actions:
+        state = reduce_guard(state, action)
     return 0
+
+
+def reduce_guard(state, action):
+    time_stamp, action_type, guard_id = parse_action(action)
+    return state
+
+
+def parse_action(action):
+    m = re.search(r"\[(?P<time_str>.*?)\] (?P<action>.*)", action)
+    time_stamp = datetime.strptime(m.group('time_str'), '%Y-%m-%d %H:%M')
+
+    action_type = ''
+    guard_id = None
+
+    if m.group('action') == 'wakes up':
+        action_type = 'wake'
+    elif m.group('action') == 'falls asleep':
+        action_type = 'sleep'
+    else:
+        m = re.search(r"Guard #(?P<guard_id>\d+) begins shift", action)
+        guard_id = int(m.group('guard_id'))
+        action_type = 'shift_change'
+
+    return time_stamp, action_type, guard_id
--- advent-of-code-2018/day4/guard_test.py
+++ advent-of-code-2018/day4/guard_test.py
@@ -1,23 +1,27 @@
-from guard import guard
+from datetime import datetime
+
+import pytest
+
+from guard import guard, parse_action
 
 
 def test_guard():
     test_input = [
-        "[1518-11-01 00:00] Guard",
+        "[1518-11-01 00:00] Guard #10 begins shift",
         "[1518-11-01 00:05] falls asleep",
         "[1518-11-01 00:25] wakes up",
         "[1518-11-01 00:30] falls asleep",
         "[1518-11-01 00:55] wakes up",
-        "[1518-11-01 23:58] Guard",
+        "[1518-11-01 23:58] Guard #99 begins shift",
         "[1518-11-02 00:40] falls asleep",
         "[1518-11-02 00:50] wakes up",
-        "[1518-11-03 00:05] Guard",
+        "[1518-11-03 00:05] Guard #10 begins shift",
         "[1518-11-03 00:24] falls asleep",
         "[1518-11-03 00:29] wakes up",
-        "[1518-11-04 00:02] Guard",
+        "[1518-11-04 00:02] Guard #99 begins shift",
         "[1518-11-04 00:36] falls asleep",
         "[1518-11-04 00:46] wakes up",
-        "[1518-11-05 00:03] Guard",
+        "[1518-11-05 00:03] Guard #99 begins shift",
         "[1518-11-05 00:45] falls asleep",
         "[1518-11-05 00:55] wakes up",
     ]
@@ -25,3 +29,22 @@ def test_guard():
     actual = guard(test_input)
 
     assert actual == 240
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("[1518-11-01 00:00] Guard #10 begins shift",
+         (datetime(1518, 11, 1, 0, 0), "shift_change", 10)),
+        ("[1518-11-01 00:05] falls asleep",
+         (datetime(1518, 11, 1, 0, 5), "sleep", None)),
+        ("[1518-11-01 00:25] wakes up",
+         (datetime(1518, 11, 1, 0, 25), "wake", None)),
+        ("[1518-11-01 00:55] Guard #99 begins shift",
+         (datetime(1518, 11, 1, 0, 55), "shift_change", 99)),
+    ]
+)
+def test_parse_action(test_input, expected):
+    actual = parse_action(test_input)
+
+    assert actual == expected
