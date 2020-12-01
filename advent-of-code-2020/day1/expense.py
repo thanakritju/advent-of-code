76ec701eb7a72cdb1e2561c829e7a68bce7aa01b
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-12-02 00:19:53 +0700

    Refactor 2020 day 1

--- advent-of-code-2020/day1/expense.py
+++ advent-of-code-2020/day1/expense.py
@@ -1,15 +1,16 @@
-def get_sum_of_two_entries(expense_report):
-    length = len(expense_report)
-    for index in range(length):
-        for another in range(index + 1, length):
-            if expense_report[index] + expense_report[another] == 2020:
-                return expense_report[index] * expense_report[another]
+from typing import Sequence
 
 
-def get_sum_of_three_entries(expense_report):
-    length = len(expense_report)
-    for index in range(length):
-        for another in range(index + 1, length):
-            for yet_another in range(index + 2, length):
-                if expense_report[index] + expense_report[another] + expense_report[yet_another] == 2020:
-                    return expense_report[index] * expense_report[another] * expense_report[yet_another]
+def get_sum_of_two_entries(expense_report: Sequence[int]):
+    for index, expense in enumerate(expense_report):
+        for another_expense in expense_report[:index]:
+            if expense + another_expense == 2020:
+                return expense * another_expense
+
+
+def get_sum_of_three_entries(expense_report: Sequence[int]):
+    for index, expense in enumerate(expense_report):
+        for another_index, another_expense in enumerate(expense_report[:index]):
+            for yet_another_expense in expense_report[:another_index]:
+                if expense + another_expense + yet_another_expense == 2020:
+                    return expense * another_expense * yet_another_expense
