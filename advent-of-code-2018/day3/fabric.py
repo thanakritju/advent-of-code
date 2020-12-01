6d01c3e6116a263e2cbdc1d8eada723afea5ff94
thanakritju <thanakrit.ju.work@gmail.com>
2020-12-01 10:02:22 +0700

    my code works but too slow

--- advent-of-code-2018/day3/fabric.py
+++ advent-of-code-2018/day3/fabric.py
@@ -1,2 +1,32 @@
+import re
+
+
 def get_overlap(fabrics):
-    return 0
+    intersections = set()
+    length = len(fabrics)
+    for index in range(length):
+        for another_index in range(index + 1, length):
+            intersections.update(get_intersection(
+                fabrics[index], fabrics[another_index]))
+    return len(intersections)
+
+
+def is_overlap(fabric, another_fabric):
+    return bool(get_intersection(fabric, another_fabric))
+
+
+def get_intersection(fabric, another_fabric):
+    return extract_input(fabric).intersection(extract_input(another_fabric))
+
+
+def extract_input(fabric):
+    m = re.search(
+        r"#\d+ @ (?P<x_start>\d+),(?P<y_start>\d+): (?P<width>\d+)x(?P<height>\d+)", fabric)
+    fabric_set = set()
+    x_start, y_start, width, height = int(m.group('x_start')), int(
+        m.group('y_start')), int(m.group('width')), int(m.group('height'))
+    for x_index in range(width):
+        for y_index in range(height):
+            fabric_set.add((x_start + x_index, y_start + y_index))
+
+    return fabric_set
--- advent-of-code-2018/day3/fabric_test.py
+++ advent-of-code-2018/day3/fabric_test.py
@@ -1,4 +1,6 @@
-from fabric import get_overlap
+import pytest
+
+from fabric import get_overlap, is_overlap, extract_input
 
 
 def test_get_overlap():
@@ -13,9 +15,38 @@ def test_get_overlap():
     assert actual == 4
 
 
+@pytest.mark.parametrize("test_input,expected",
+                         [
+                             (("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4"), True),
+                             (("#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"), False),
+                             (("#1 @ 1,3: 4x4", "#3 @ 5,5: 2x2"), False),
+                         ]
+                         )
+def test_is_overlap(test_input, expected):
+    actual = is_overlap(test_input[0], test_input[1])
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [
+                             ("#1 @ 1,3: 2x2",
+                              set([(1, 3), (1, 4), (2, 3), (2, 4)])),
+                             ("#2 @ 3,1: 2x2",
+                              set([(3, 1), (4, 1), (3, 2), (4, 2)])),
+                             ("#3 @ 1,1: 1x2", set([(1, 1), (1, 2)])),
+                             ("#4 @ 1,0: 2x1", set([(1, 0), (2, 0)])),
+                         ]
+                         )
+def test_extract_input(test_input, expected):
+    actual = extract_input(test_input)
+
+    assert actual == expected
+
+
 def test_get_overlap_for_puzzle_input():
     puzzle_input = open("advent-of-code-2018/day3/fabrics.txt", "r")
-    content = puzzle_input.read().split()
+    content = puzzle_input.read().splitlines()
 
     actual = get_overlap(content)
 
