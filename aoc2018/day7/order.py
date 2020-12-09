4f86bd5afc80178a471ff405d0e5426afcbbe0a7
thanakritju <thanakrit.ju.work@gmail.com>
2020-12-09 20:20:53 +0700

    Day7 2018 im not going to touch this again so painful

--- aoc2018/day7/order.py
+++ aoc2018/day7/order.py
@@ -1,26 +1,103 @@
 import re
 from typing import Sequence, Tuple
+import string
+import time
 
 
-def order(instructions: Sequence[str]) -> str:
+class Task:
+    def __init__(self, time_need, name=""):
+        self.name = name
+        self.start_time = None
+        self.time_need = time_need
+
+    def process(self, current_time):
+        self.start_time = current_time
+
+    def is_finished(self, current_time):
+        return current_time - self.start_time >= self.time_need + string.ascii_uppercase.find(self.name)
+
+    def is_empty(self):
+        return self.name == ""
+
+
+def order(instructions: Sequence[str], worker_number=1, time_need=1) -> str:
     edges = set(map(parse_input, instructions))
+    out, time = process_job(edges, worker_number, time_need)
+    return "".join(out), time
+
+
+def process(queue: Sequence[str], out: Sequence[str], edges: Tuple[str, str]) -> Tuple[Sequence[str], Sequence[str]]:
+    node = queue.pop(0)
+    prerequisites = [edge[0] for edge in edges if node == edge[1]]
+    if(all(each in out for each in prerequisites)):
+        out.append(node)
+        queue += [edge[1] for edge in edges if node ==
+                  edge[0] and edge[1] not in queue]
+        queue = sorted(queue)
+    else:
+        queue.append(node)
+    return queue, out
+
+
+def process_job(edges: Tuple[str, str], worker_number: int, time_need: int) -> Sequence[str]:
+    out = []
+    main_queue = get_first_tasks(edges)
+    seconds = 0
+    if worker_number == 1:
+        while len(main_queue) != 0:
+            main_queue, out = process(main_queue, out, edges)
+        return out, None
+    else:
+        empty_tasks = [Task(time_need) for each in range(worker_number)]
+        tasks, main_queue = fill_tasks(empty_tasks, main_queue, seconds)
+        is_all_done = False
+        while len(main_queue) != 0 or not is_all_done:
+            main_queue, out, tasks = process_task(
+                main_queue, out, tasks, edges, seconds, time_need)
+            seconds += 1
+            is_all_done = all([task.is_empty() for task in tasks])
+            # print(f"out {out}, main_queue {main_queue}, seconds {seconds}")
+            time.sleep(0.001)
+        return out, seconds - 1
+
+
+def process_task(main_queue, out, tasks: Sequence[Task], edges, seconds, time_need):
+    for i, task in enumerate(tasks):
+        prerequisites = [edge[0] for edge in edges if task.name == edge[1]]
+        # print(f"worker {i}, task {task.name}, task {task.name}, seconds {seconds}")
+        if not task.is_empty() and task.is_finished(seconds) and all(each in out for each in prerequisites):
+            out.append(task.name)
+            main_queue += get_next_tasks(task, main_queue, tasks, edges, out)
+            tasks[i] = Task(time_need)
+            main_queue = sorted(main_queue)
+
+    tasks, main_queue = fill_tasks(tasks, main_queue, seconds)
+    return main_queue, out, tasks
+
+
+def get_next_tasks(finished_task, main_queue, tasks, edges, out):
+    return [
+        edge[1] for edge in edges
+        if finished_task.name == edge[0] and
+        edge[1] not in main_queue and
+        edge[1] not in list(map(lambda x: x.name, tasks)) and
+        all(each in out for each in [another_edge[0]
+                                     for another_edge in edges if edge[1] == another_edge[1]])
+    ]
+
+
+def get_first_tasks(edges):
     sources = set(map(lambda x: x[0], edges))
     dests = set(map(lambda x: x[1], edges))
+    return sorted(list(sources.difference(dests)))
 
-    queue = sorted(list(sources.difference(dests)))
-    out = []
-    while (len(queue) != 0):
-        node = queue.pop(0)
-        prerequisites = [edge[0] for edge in edges if node == edge[1]]
-        if(all(each in out for each in prerequisites)):
-            out.append(node)
-            queue += [edge[1] for edge in edges if node ==
-                      edge[0] and edge[1] not in queue]
-            queue = sorted(queue)
-        else:
-            queue.append(node)
 
-    return "".join(out)
+def fill_tasks(tasks, main_queue, seconds):
+    for i in range(len(tasks)):
+        if tasks[i].is_empty() and len(main_queue) != 0:
+            tasks[i].name = main_queue.pop(0)
+            tasks[i].process(seconds)
+    return tasks, main_queue
 
 
 def parse_input(instruction: str) -> Tuple[str, str]:
--- aoc2018/day7/order_test.py
+++ aoc2018/day7/order_test.py
@@ -1,6 +1,9 @@
+import pytest
+
 from order import order
 
 
+@pytest.mark.timeout(1)
 def test_order():
     test_input = [
         "Step C must be finished before step A can begin.",
@@ -12,15 +15,38 @@ def test_order():
         "Step F must be finished before step E can begin.",
     ]
 
-    actual = order(test_input)
+    actual, time = order(test_input)
 
     assert actual == "CABDFE"
 
 
+@pytest.mark.timeout(1)
+def test_order_with_workers():
+    test_input = [
+        "Step C must be finished before step A can begin.",
+        "Step C must be finished before step F can begin.",
+        "Step A must be finished before step B can begin.",
+        "Step A must be finished before step D can begin.",
+        "Step B must be finished before step E can begin.",
+        "Step D must be finished before step E can begin.",
+        "Step F must be finished before step E can begin.",
+    ]
+
+    actual, time = order(test_input, worker_number=2, time_need=1)
+
+    assert actual == "CABFDE"
+    assert time == 15
+
+
+@pytest.mark.timeout(5)
 def test_order_for_puzzle_input():
     puzzle_input = open("aoc2018/day7/instructions.txt", "r")
     content = puzzle_input.read().splitlines()
 
-    actual = order(content)
+    actual, time = order(content)
 
     assert actual == "OCPUEFIXHRGWDZABTQJYMNKVSL"
+
+    actual, time = order(content, worker_number=5, time_need=61)
+
+    assert time == 991
