2b6ac76794e52e80332a9b7cb0fceb28134ab0e2
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-12-19 00:06:56 +0700

    finish 2020 day 18 it is fun

---
+++ aoc2020/day18/math.py
@@ -0,0 +1,69 @@
+
+
+T_LPAREN = "("
+T_RPAREN = ")"
+T_OPERATIONS = "+*"
+T_NUMBER = "0123456789"
+
+Operations = {
+    T_OPERATIONS[0]: (lambda a, b: a + b),
+    T_OPERATIONS[1]: (lambda a, b: a * b),
+}
+
+
+def calculate(expressions, precedence=False):
+    tokens = expressions.replace("(", "( ").replace(")", " )").split(" ")
+    output_queue = []
+    operator_stacks = []
+    while tokens:
+        token = tokens.pop(0)
+        if is_sym(token, T_NUMBER):
+            output_queue.append(token)
+        elif is_sym(token, T_OPERATIONS):
+            while (
+                operator_stacks and
+                not is_sym(operator_stacks[-1], T_LPAREN) and
+                is_greater_precedence(operator_stacks[-1], token, precedence)
+            ):
+                output_queue.append(operator_stacks.pop())
+            operator_stacks.append(token)
+        elif is_sym(token, T_LPAREN):
+            operator_stacks.append(token)
+        elif is_sym(token, T_RPAREN):
+            while not is_sym(operator_stacks[-1], T_LPAREN):
+                output_queue.append(operator_stacks.pop())
+            if is_sym(operator_stacks[-1], T_LPAREN):
+                operator_stacks.pop()
+
+    while operator_stacks:
+        output_queue.append(operator_stacks.pop())
+
+    stack = []
+    for token in output_queue:
+        if is_operator(token):
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = Operations[token](arg1, arg2)
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
+
+
+def is_greater_precedence(operator_at_top_stacks, operator, feature_flag):
+    if not feature_flag:
+        return True
+    else:
+        operator_at_top_stacks_level = T_OPERATIONS.find(
+            operator_at_top_stacks)
+        operator_level = T_OPERATIONS.find(operator)
+        return operator_at_top_stacks_level < operator_level
+
+
+def is_sym(token, symbol):
+    return token in symbol
+
+
+def is_operator(token):
+    return is_sym(token, T_OPERATIONS)
---
+++ aoc2020/day18/math_test.py
@@ -0,0 +1,51 @@
+import pytest
+
+from aoc2020.day18.math import *
+
+
+@pytest.mark.parametrize(
+    "expressions, expected",
+    [
+        ('2 * 3', 6),
+        ('2 + 3', 5),
+        ('2 + 3 * 5', 25),
+        ('2 * 3 + (4 * 5)', 26),
+        ('5 + (8 * 3 + 9 + 3 * 4 * 3)', 437),
+        ('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 12240),
+        ('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 13632),
+    ]
+)
+def test_calculate(expressions, expected):
+    actual = calculate(expressions)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "expressions, expected",
+    [
+        ('1 + (2 * 3) + (4 * (5 + 6))', 51),
+        ('2 + 3 * 5', 25),
+        ('2 * 3 + (4 * 5)', 46),
+        ('5 + (8 * 3 + 9 + 3 * 4 * 3)', 1445),
+        ('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 669060),
+        ('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 23340),
+    ]
+)
+def test_calculate(expressions, expected):
+    actual = calculate(expressions, True)
+
+    assert actual == expected
+
+
+def test_calculate_for_puzzle_input():
+    puzzle_input = open("aoc2020/day18/input.txt", "r")
+    content = puzzle_input.read().splitlines()
+
+    actual = sum(map(calculate, content))
+
+    assert actual == 6640667297513
+
+    actual = sum([calculate(line, True) for line in content])
+
+    assert actual == 6640667297513
