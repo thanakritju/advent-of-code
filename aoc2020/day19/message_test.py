8b3f75f24286e1c8cc838c05bbfb4b43019d95b5
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-12-20 00:05:36 +0700

    Add failing test see you tmr formal language

--- aoc2020/day19/message_test.py
+++ aoc2020/day19/message_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from aoc2020.day19.message import *
 
 
@@ -14,10 +16,39 @@ def test_get_valid_messages():
     assert actual == 2
 
 
+@pytest.mark.timeout(1)
+def test_get_valid_messages_2():
+    puzzle_input = open("aoc2020/day19/sample2.txt", "r")
+    rules, messages = puzzle_input.read().split("\n\n")
+
+    actual = get_valid_messages(rules.splitlines(), messages.splitlines())
+
+    assert actual == 3
+
+    rules = rules.replace("8: 42", "8: 42 | 42 8").replace(
+        "11: 42 31", "11: 42 31 | 42 11 31")
+
+    actual = get_valid_messages(rules.splitlines(), messages.splitlines())
+
+    assert actual == 12
+
+
 def test_get_valid_messages_for_puzzle_input():
     puzzle_input = open("aoc2020/day19/input.txt", "r")
     rules, messages = puzzle_input.read().split("\n\n")
 
     actual = get_valid_messages(rules.splitlines(), messages.splitlines())
 
-    assert actual == 2
+    assert actual == 102
+
+
+@pytest.mark.timeout(1)
+def test_get_valid_messages_for_puzzle_input_part2():
+    puzzle_input = open("aoc2020/day19/input.txt", "r")
+    rules, messages = puzzle_input.read().split("\n\n")
+    rules = rules.replace("8: 42", "8: 42 | 42 8").replace(
+        "11: 42 31", "11: 42 31 | 42 11 31")
+
+    actual = get_valid_messages(rules.splitlines(), messages.splitlines())
+
+    assert actual == 0
