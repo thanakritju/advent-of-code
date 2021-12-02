2012438425e06e86ff0ebd129a5deb668a9f48b1
thanakritju <thanakrit.ju.work@gmail.com>
2021-12-02 16:12:09 +0700

    finish part2 day2

--- aoc2021/day2/submarine.py
+++ aoc2021/day2/submarine.py
@@ -13,3 +13,21 @@ def get_multiplication_of_coordinate(actions):
             y -= int(value)
         
     return x*y 
+
+def get_multiplication_of_coordinate_with_aim(actions):
+    x = 0
+    y = 0
+    aim = 0
+
+    for action in actions:
+        command, value = action.split(" ")
+
+        if command == "forward":
+            x += int(value)
+            y += aim * int(value)
+        if command == "down":
+            aim += int(value)
+        if command == "up":
+            aim -= int(value)
+        
+    return x*y 
--- aoc2021/day2/submarine_test.py
+++ aoc2021/day2/submarine_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc2021.day2.submarine import get_multiplication_of_coordinate
+from aoc2021.day2.submarine import get_multiplication_of_coordinate, get_multiplication_of_coordinate_with_aim
 
 
 def test_get_multiplication_of_coordinate():
@@ -26,3 +26,28 @@ def test_get_multiplication_of_coordinate_for_puzzle():
     actual = get_multiplication_of_coordinate(content)
 
     assert actual == 2036120
+
+
+def test_get_multiplication_of_coordinate_with_aim():
+    test_input = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ]
+
+    actual = get_multiplication_of_coordinate_with_aim(test_input)
+
+    assert actual == 900
+
+
+@pytest.mark.puzzle
+def test_get_multiplication_of_coordinate_with_aim_for_puzzle():
+    puzzle_input = open("aoc2021/day2/input.txt", "r")
+    content = list(map(str, puzzle_input.read().splitlines()))
+
+    actual = get_multiplication_of_coordinate_with_aim(content)
+
+    assert actual == 2015547716
