fb543d59e91d20e07a11be5479764210e500da4b
thanakritju <thanakrit.ju.work@gmail.com>
2021-12-03 20:36:17 +0700

    finish 2018 day 8

--- aoc2018/day8/memory_maneuver.py
+++ aoc2018/day8/memory_maneuver.py
@@ -1,8 +1,7 @@
 class Node:
-    def __init__(self, parent=None):
+    def __init__(self):
         self.children = []
         self.metadata_entries = []
-        self.parent = parent
 
     def add_child(self, child):
         self.children.append(child)
@@ -10,22 +9,46 @@ class Node:
     def add_metadata(self, entry):
         self.metadata_entries.append(entry)
 
+    def sum(self):
+        return sum(self.metadata_entries) + sum([child.sum() for child in self.children])
+
+    def value(self):
+        if len(self.children) == 0:
+            return sum(self.metadata_entries)
+        else:
+            return sum([self.children[each - 1].value() for each in self.metadata_entries if each <= len(self.children)])
+
+
+class Index:
+    def __init__(self):
+        self.value = -1
+
+    def next(self):
+        self.value += 1
+        return self.value
+
 
 def get_sum_of_all_metadata_entries(records):
-    current_metadata_entry = 0
-    current_childrens = []
-    current_node = Node()
-    n = len(records)
-    index = 0
+    index = Index()
+    node = read_node(records, index)
+    return node.sum()
 
-    while index != n:
-        if current_metadata_entry == 0:
-            current_childrens.append(records[index])
-            current_metadata_entry = records[index+1]
 
-            index += 2
-        else:
-            current_node.add_metadata(records[index])
+def get_sum_of_all_metadata_entries_v2(records):
+    index = Index()
+    node = read_node(records, index)
+    return node.value()
 
-            index += 1
-            current_metadata_entry -= 1
+
+def read_node(numbers, index):
+    node = Node()
+    children = numbers[index.next()]
+    metadata = numbers[index.next()]
+
+    for _ in range(children):
+        node.add_child(read_node(numbers, index))
+
+    for _ in range(metadata):
+        node.add_metadata(numbers[index.next()])
+
+    return node
--- aoc2018/day8/memory_maneuver_test.py
+++ aoc2018/day8/memory_maneuver_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc2018.day8.memory_maneuver import get_sum_of_all_metadata_entries
+from aoc2018.day8.memory_maneuver import get_sum_of_all_metadata_entries, get_sum_of_all_metadata_entries_v2
 
 
 def test_get_multiplication_of_coordinate():
@@ -18,5 +18,24 @@ def test_get_multiplication_of_coordinate_for_puzzle():
     content = list(map(int, puzzle_input.read().split(" ")))
 
     actual = get_sum_of_all_metadata_entries(content)
+
+    assert actual == 44338
+
+
+def test_get_multiplication_of_coordinate_v2():
+    puzzle_input = open("aoc2018/day8/sample.txt", "r")
+    content = list(map(int, puzzle_input.read().split(" ")))
+
+    actual = get_sum_of_all_metadata_entries_v2(content)
+
+    assert actual == 66
+
+
+@pytest.mark.puzzle
+def test_get_multiplication_of_coordinate_v2_for_puzzle():
+    puzzle_input = open("aoc2018/day8/input.txt", "r")
+    content = list(map(int, puzzle_input.read().split(" ")))
+
+    actual = get_sum_of_all_metadata_entries_v2(content)
 
     assert actual == 2036120
